27e6b39ee7befecb0797a54030b4ddaabfb7773b
Victor <ban.victor728@gmail.com>
2024-08-15 14:35:23 -0400

    created ternary to render either single planet or all planets. Clicking any planet should retrieve planet detail & then change state to display single planet; two buttons for reseting display state to back to all planets

--- src/Components/Planets.jsx
+++ src/Components/Planets.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 
 const Planetdetails = () => {
   const [allPlanets, setAllPlanets] = useState([]);
+  const [planetDetails, setPlanetDetails] = useState({});
+  const [planetReviews, setPlanetReviews] = useState([]);
+  const [showPlanet, setShowPlanet] = useState(false);
 
   useEffect(()=> {
     try{
@@ -20,13 +23,33 @@ const Planetdetails = () => {
   return (
     <main>
       {
-        allPlanets.map((singlePlanet) =>{
-          return (<a> {/* probably not actually an anchor tag*/}
+        showPlanet? <>
+          {singlePlanet.map((planet) => {
+            return (<>
+                <button onClick={()=>setShowPlanet(false)}> See other Planets </button>
+                <h2>{planet.name}</h2> <br />
+                <p> {[planet.description]}</p>
+                  <p> {planetReviews.map(review => {
+                    <>
+                    <h4> Reviews </h4>
+                    <p key={review.id}> {review.review}</p>
+                    </>
+                })}</p>
+                <p> This Planet is owned by: {planet.ownedby} </p>
+                {/* image goes here */}
+                <button >Add this Planet to Cart</button>
+                <button onClick={()=>setShowPlanet(false)}>Go Back to Exploring all Planets</button>
+              </>
+            );
+          })};
+          </>
+        :
+        allPlanets.map(singlePlanet => {
+          return (<a> {/* probably not actually an anchor tag, when this element is click singlePlanet State changed to true*/}
             <h3 key={singlePlanet.id}> {singlePlanet.name} </h3> <br /> 
             {/* image goes here */}
             </a>)
-        }
-        )
+        })
       }
     </main>
   )
