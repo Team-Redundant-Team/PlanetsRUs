e3dbdb171c5ebb53c7bd129d30be51d48a126106
Robin Singh <vougishtiger@gmail.com>
2024-08-15 16:08:22 -0700

    created change password button/function

--- src/UserAccount.jsx
+++ src/UserAccount.jsx
@@ -2,20 +2,37 @@ import React from 'react';
 import {useState} from 'react';
 import axios from 'axios';
 
-const UserAccount = () => {
-  const [emailInput, setEmailInput]= useState('')
 
-    const changeEmail= async(emailInput)=> {
+const UserAccount = () => {
+  const [emailInput, setEmailInput]= useState('');
+  const [passwordInput, setPasswordInput]= useState('');
+  const [secondaryPasswordInput, setSecondaryPasswordInput]= useState('');
+    
+  const changeEmail= async(event)=> {
+    event.preventDefault();
+    try{
+      const response= await axios.put('/api/change-email', {email: emailInput})
+    }catch(err) {
+      console.log(err);
+    }
+    
+    }
+
+  const changePW=  async(event)=> {
+    event.preventDefault();
+    if(passwordInput===secondaryPasswordInput){
       try{
-        const response= await axios.put('/api/change-email', {email: emailInput})
+        const response= await axios.put('/api/change-pw', {password: passwordInput})
       }catch(err) {
-          console.log(err);
+        console.log(err)
         }
+      }
+      else{
+        alert('Passwords do not match, Please try again!');
+      }
     
     }
     
-    
-    console.log("changeEmail function called");
 
 
   return (
@@ -23,7 +40,8 @@ const UserAccount = () => {
       <h1>User Account</h1>
       <input type="email" value={emailInput} onChange={(event)=>{setEmailInput(event.target.value)}} placeholder='"Enter New Email' />
       <button onClick={()=>{changeEmail()}}>Change Email</button>
-      <h2>Change Password</h2>
+      <input type="password" value={emailInput} onChange={(event)=>{setPasswordInput(event.target.value)}} placeholder='"Enter New Password' />
+      <button onClick={()=>{changePW()}}>Change Password</button>
       <h2>Planets Owned</h2>
       <ul>
         {/* List of planets owned can go here */}
