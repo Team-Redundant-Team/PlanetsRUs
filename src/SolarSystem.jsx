0ce928fb507ffdfbf2babde369103d413251320c
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-08-15 16:28:12 -0700

    fixed camera positioning

--- src/SolarSystem.jsx
+++ src/SolarSystem.jsx
@@ -16,7 +16,7 @@ class SolarSystem {
   }
 
   createStar() {
-    const starGeometry = new THREE.SphereGeometry(Math.random() * 9 + 1, 32, 32);
+    const starGeometry = new THREE.SphereGeometry(Math.random() * 20 + 7, 32, 32);
     const starMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
     const star = new THREE.Mesh(starGeometry, starMaterial);
   
