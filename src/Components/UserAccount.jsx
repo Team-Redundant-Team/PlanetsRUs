188147947515fc095964500a39f57470f3fbffc9
Victor <ban.victor728@gmail.com>
2024-08-15 19:56:02 -0400

    wrote useEffect to get user information via token

--- src/Components/UserAccount.jsx
+++ src/Components/UserAccount.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useState} from 'react';
 import axios from 'axios';
 import updateUserDetails from './updateUserDetails';
 
 
 const UserAccount = () => {
+  const [user, setUser] = useState("");
+
+  useEffect(()=>{
+    try{
+      const getUserData = async () => {
+        const token = req.headers.authorization.split(" ")[1];
+        const user = await axios.get('/api/users', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          } // may not need lines 15-16 due to api deconstruct in server.cjs for the call
+        })
+        setUser(user);
+      }
+      getUserData();
+    } catch (error){
+      console.log('unable to get user data');
+    }
+  },[])
 
   return (
     <>
-      <h1>User Account</h1>
+      <h1>Welcome {user.username}</h1>
         <updateUserDetails />
-      <h2>Planets Owned</h2>
+      <h2>Your Planets Owned</h2>
+      <article></article>
       <ul>
         {/* List of planets owned can go here */}
       </ul>
