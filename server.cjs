809058a1c5948b49e0d90ccf4b8b4b1926ae9cea
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-08-15 18:04:49 -0700

    created a better running build with frontend

--- server.cjs
+++ server.cjs
@@ -9,6 +9,13 @@ const { getPlanetReviews } = require('./db/reviews.cjs')
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
 // const verifyToken = (req,res,next) => {
 //   const token = req.headers["authorization"];
--- src/SolarSystem.jsx
+++ src/SolarSystem.jsx
@@ -65,7 +65,7 @@ class SolarSystem {
   addPlanet(size, color, distance) {
   const textureLoader = new THREE.TextureLoader();
   const textureIndex = Math.floor(Math.random() * 100) + 1;
-  const texturePath = `/src/textures/texture${textureIndex}.gif`;
+  const texturePath = `/public/textures/texture${textureIndex}.gif`;
 
   let texture;
   try {
